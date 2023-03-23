4e58df3dc6e05c61a10bd71aee747603cc183945
Donuts and Blood <59175438+DonutsAndBlood@users.noreply.github.com>
2023-03-22 21:23:56 -0300

    Update check.py

--- check.py
+++ check.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
-x = pd.read_csv(r"C:\Users\aluno.laboratorio\Desktop\microdados\MICRODADOS.csv", encoding='latin-1', sep=';', low_memory=False)
+x = pd.read_csv("MICRODADOS.csv", encoding='latin-1', sep=";", low_memory=False)
 
 x.columns =[column.replace(" ", "_") for column in x.columns]
 
-x.query('Municipio == "CARIACICA" \
-        and ComorbidadeTabagismo =="Sim"', inplace = True)
+x.query('Municipio == "CARIACICA"\
+        and ComorbidadeTabagismo =="Sim"\
+        and Evolucao == "Óbito pelo COVID-19"\
+        and DataObito > "2019"', inplace = True)
 
 print(x)
+
